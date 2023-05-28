f15e0e65056022d3f42c87de2b0d6b4b2af903ef
edvalenzuela <edvalenzuelap@gmail.com>
2023-05-27 20:02:59 -0400

    ✨ creation panel administration

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,6 @@
 const passport = require('passport')
+const mongoose = require('mongoose')
+const Vacante = mongoose.model('Vacante')
 
 exports.autenticarUsuario = passport.authenticate('local', {
   successRedirect: '/administracion',
@@ -6,3 +8,26 @@ exports.autenticarUsuario = passport.authenticate('local', {
   failureFlash: true,
   badRequestMessage: 'Ambos campos son obligatorios'
 })
+
+// revisar si el usuario esta autenticado o no
+exports.verificarUsuario = (req, res, next) => {
+  //revisar el usuario
+  if(req.isAuthenticated()){
+    return next(); //estan autenticados
+  }
+
+  //redireccionar
+  res.redirect('/iniciar-sesion')
+}
+
+exports.mostrarPanel = async(req, res) => {
+
+  //consultar el usuario autenticado
+  const vacantes = await Vacante.find({autor:req.user._id}).lean()
+
+  res.render('administracion', {
+    nombrePagina: 'Panel de administración',
+    tagline: 'Crea y administra tus vacantes desde aquí',
+    vacantes
+  })
+}
